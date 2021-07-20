c0d2f3c3382c611b3d66a3590adc87381cc6a94a
Alexander Friedman <webdevajf@gmail.com>
2021-07-19 18:26:02 -0700

    Initial Commit

---
+++ SaveTheKittensExceptTheHairlessOnes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SaveTheKittensExceptTheHairlessOnes
+//
+//  Created by Alexander Friedman on 7/19/21.
+//  Copyright © 2021 istm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
